5fcc20698c66d273389c54a3394cf4b65871456f
bdthanh <bdthanh200403@gmail.com>
2024-05-15 21:51:36 +0800

    fix encoder

--- src/fastspeech2/encoder.py
+++ src/fastspeech2/encoder.py
@@ -44,11 +44,11 @@ class EncoderLayer(Module):
     def forward(self, x: Tensor, mask: Tensor):
         _x = self.self_attn(x, x, x, mask)
         x = self.self_attn_layer_norm(x + self.self_attn_dropout(_x))
-        masked_x = x.masked_fill(mask==0, 0)
+        x = x.masked_fill(mask==0, 0)
         
         _x = self.feed_fwd(x)
         x = self.feed_fwd_layer_norm(x + self.feed_fwd_dropout(_x))
-        masked_x = x.masked_fill(mask==0, 0)
+        x = x.masked_fill(mask==0, 0)
         
-        return masked_x
+        return x
         
