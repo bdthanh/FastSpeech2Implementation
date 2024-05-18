Fix masking for dec/len_reg and implement training script

src/dataset/symbol_vocabulary.py
@@ -40,5 +40,5 @@ class SymbolVocabulary:
         return len(self.symbol_to_id)
     
     def symbols_to_ids(self, symbols_list: List[str]):
-        return torch.tensor(list(map(lambda symbol: self.symbol_to_id[symbol], symbols_list)), dtype=torch.int64)
+        return np.array(list(map(lambda symbol: self.symbol_to_id[symbol], symbols_list)))
     

src/model/components/length_regulator.py
@@ -12,19 +12,19 @@ class LengthRegulator(Module):
     def forward(self, x: Tensor, x_pred_dur, max_dur: int = None):
         #x: [B, seq_len, dim]
         #x_len_ratio: [B, seq_len]
-        output, mel_dur = [], []
+        output, mel_durs = [], []
         for inner_x, pred_dur in zip(x, x_pred_dur):
             out = []
             for i, inner_x_item in enumerate(inner_x):
                 out.extend(inner_x_item.repeat(pred_dur[i], 1))
                 out = torch.cat(out, 0)
             output.append(out)
-            mel_dur.append(out.size(0))
+            mel_durs.append(out.size(0))
         if max_dur is not None:
             output = _pad(output, max_dur)
         else: # pad to the longest length in the batch
             output = _pad(output)
-        return output, torch.Tensor(mel_dur)
+        return output, torch.Tensor(mel_durs)
     
     
 def _pad(x, mel_max_dur=None):

src/model/variance_adaptor.py
@@ -4,7 +4,7 @@ from torch import Tensor
 from torch.nn import Module, Parameter
 from components.variance_predictor import VariancePredictor
 from components.length_regulator import LengthRegulator
-from utils import load_json
+from utils import load_json, get_mask_from_lengths
 
 class VarianceAdaptor(Module):
     
@@ -41,11 +41,12 @@ class VarianceAdaptor(Module):
     
             
     def forward(self, x: Tensor, pitch_trg: Tensor, energy_trg: Tensor = None, src_mask: Tensor = None, 
-                mel_mask: Tensor = None,max_dur: int = None, p_control: float = 1.0, e_control: float = 1.0, 
+                mel_mask: Tensor = None, max_dur: int = None, p_control: float = 1.0, e_control: float = 1.0, 
                 d_control: float = 1.0):
         log_dur_pred = self.duration_predictor(x, src_mask)
         dur_rounded = torch.clamp(torch.round((torch.exp(log_dur_pred) - 1) * d_control), min=0)
-        x = self.length_regulator(x, dur_rounded, max_dur)
+        x, mel_durs = self.length_regulator(x, dur_rounded, max_dur)
+        mel_mask = get_mask_from_lengths(mel_durs) #get new mask after length regulation
         
         
         pitch_pred = self.pitch_predictor(x, mel_mask) * p_control
@@ -62,5 +63,5 @@ class VarianceAdaptor(Module):
             energy_emb = self.energy_embedding(torch.bucketize(energy_pred, self.energy_bins))
         x = x + energy_pred
         
-        return (x, log_dur_pred, dur_rounded, pitch_pred, pitch_emb, energy_pred, energy_emb)        
+        return (x, mel_mask, log_dur_pred, dur_rounded, pitch_pred, pitch_emb, energy_pred, energy_emb)        
         

train.py
@@ -4,6 +4,7 @@ import torch
 import wandb
 from pathlib import Path
 from tqdm import tqdm
+from torch import nn 
 from torch.utils.data import DataLoader
 from src.optimizer.optimizer import ScheduledOptim, get_optimizer
 from src.dataset.parallel_dataset import ParallelDataset
@@ -12,14 +13,14 @@ from src.model.fastspeech2 import FastSpeech2, get_fastspeech2
 from src.model.fastspeech2_loss import FastSpeech2Loss
 from src.utils import choose_device, load_config, create_if_missing_folder, is_file_exist, get_num_params, get_mask_from_lengths
 
-def save_checkpoint(path, model, optimizer, epoch, global_step):
+def save_checkpoint(path, model: FastSpeech2, optimizer:ScheduledOptim, epoch, global_step):
     torch.save({
         'epoch': epoch,
         'global_step': global_step,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict()
     }, path)
-    
+
 def load_checkpoint_if_exists(path, model: FastSpeech2, optimizer: ScheduledOptim):
     initial_epoch, global_step = 0, 0
     if is_file_exist(path):
@@ -45,6 +46,25 @@ def get_ds(config):
     )
     return symbol_vocab, train_loader, valid_loader
 
+def unpack_batch(batch, device):
+    ids = batch['ids']
+    raw_texts = batch['raw_texts']
+    speakers = torch.from_numpy(batch['speakers']).long().to(device)
+    phonemes = torch.from_numpy(batch['phonemes']).long().to(device)
+    phonemes_lens = torch.from_numpy(batch['phonemes_lens']).to(device)
+    max_phoneme_len = batch['max_phoneme_len']
+    mels = torch.from_numpy(batch['mels']).float().to(device)
+    mel_lens = torch.from_numpy(batch['mel_lens']).to(device)
+    max_mel_len = batch['max_mel_len']
+    pitches = torch.from_numpy(batch['pitches']).float().to(device)
+    energies = torch.from_numpy(batch['energies']).float().to(device)
+    durations = torch.from_numpy(batch['durations']).long().to(device)
+
+    return ids, raw_texts, speakers, phonemes, phonemes_lens, max_phoneme_len, mels, mel_lens, max_mel_len, pitches, energies, durations
+
+def epoch_eval(model: FastSpeech2, global_step: int, epoch: int, val_dataloader: DataLoader, device):
+    pass
+
 def train(config):
     device = choose_device()
     symbol_vocab, train_loader, valid_loader = get_ds(config)
@@ -56,7 +76,7 @@ def train(config):
     )
     num_params = get_num_params(model)
     print(f"Number of FastSpeech2 parameter is: {num_params}")
-    loss = FastSpeech2Loss()
+    loss_func = FastSpeech2Loss()
     wandb.define_metric("global_step")
     wandb.define_metric("validation/*", step_metric="global_step")
     wandb.define_metric("train/*", step_metric="global_step")
@@ -74,8 +94,37 @@ def train(config):
         model.train()
         batch_iter = tqdm(train_loader, desc=f"Processing Epoch {epoch:02d}", total=len(train_loader))
         for batch in batch_iter:
-            optimizer.zero_grad()            
-    
+            optimizer.zero_grad() 
+            ids, raw_texts, speakers, phonemes, phonemes_lens, max_phoneme_len, mel_trg, mel_lens, max_mel_len, pitch_trg, energy_trg, dur_trg = unpack_batch(batch, device)
+            src_mask = get_mask_from_lengths(phonemes_lens, max_phoneme_len)
+            mel_mask = get_mask_from_lengths(mel_lens, max_mel_len)
+            #need 2 mel_mask for pred duration
+            mel_pred, mel_mask, postnet_mel_pred, log_dur_pred, dur_rounded, pitch_pred, pitch_emb, energy_pred, energy_emb = model(
+                phonemes, src_mask, mel_mask, pitch_trg, energy_trg
+            )    
+            total_loss, mel_loss, mel_postnet_loss, dur_loss, pitch_loss, energy_loss = loss_func(
+                mel_trg, dur_trg, pitch_trg, energy_trg, mel_pred, postnet_mel_pred, log_dur_pred, pitch_pred, energy_pred, src_mask, mel_mask
+            )
+            total_loss = total_loss / grad_acc_step
+            
+            wandb.log({'train/total_loss': total_loss.item(), 'global_step': global_step})
+            wandb.log({'train/mel_loss': mel_loss.item(), 'global_step': global_step})
+            wandb.log({'train/mel_postnet_loss': mel_postnet_loss.item(), 'global_step': global_step})
+            wandb.log({'train/dur_loss': dur_loss.item(), 'global_step': global_step})
+            wandb.log({'train/pitch_loss': pitch_loss.item(), 'global_step': global_step})
+            wandb.log({'train/energy_loss': energy_loss.item(), 'global_step': global_step})
+            
+            total_loss.backward()
+            if global_step % grad_acc_step == 0:
+                # Clipping gradients to avoid gradient explosion
+                nn.utils.clip_grad_norm_(model.parameters(), grad_clip_thresh)
+
+                # Update weights
+                optimizer.step_and_update_lr()
+            global_step+=1
+        epoch_eval(model, global_step, epoch, valid_loader, device)
+        save_checkpoint(config['path']['checkpoint_last'], model, optimizer, epoch, global_step)
+            
 
 if __name__ == '__main__':
     current_file_path = Path(__file__).resolve() 